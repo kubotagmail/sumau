08aa48f335c9b37996ced20377db8244477b5e15
EC2 Default User <k.nulaw20.gm.9406@gmail.com>
2022-11-16 14:06:28 +0000

    [Update]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,4 +13,20 @@ class Customer < ApplicationRecord
   #   super && (is_deleted == false)
   # end
 
+  # ゲストログイン用に作成した
+  def self.guest
+    find_or_create_by!(last_name: 'guest',first_name: 'user' ,email: 'guest@example.com') do |customer|
+      # find_or_create_by!で上記のデータがあるか、存在しない場合は下記を作成
+      customer.password = SecureRandom.urlsafe_base64
+      customer.last_name = "guest"
+      customer.first_name = "user"
+      customer.last_name_kana = "ゲスト"
+      customer.first_name_kana = "ユーザー"
+      customer.postal_code = "0010001"
+      customer.address = "guestuser.address1234567890"
+      customer.telephone_number = "0000000000"
+      customer.is_deleted = false
+    end
+  end
+
 end
