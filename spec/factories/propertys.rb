73aae6c07bca791c57190a134d109e7fc596db5d
EC2 Default User <k.nulaw20.gm.9406@gmail.com>
2022-12-17 12:21:39 +0000

    [Update]

---
+++ spec/factories/propertys.rb
@@ -0,0 +1,19 @@
+# 参考記事
+# https://qiita.com/tofuonfire/items/2ee10bed7a4be3d142fb
+# https://qiita.com/Hyuga-Tsukui/items/57fa77df2b2942414307
+FactoryBot.define do
+  factory :property do
+    customer_id {1}
+    property_type_id {1}
+    floor_plan_id {1}
+    location { Faker::Lorem.characters(number:20) }
+    description { Faker::Lorem.characters(number:50) }
+    price { Faker::Number.number(digits: 4) }
+    sales_status {0}
+    star {0}
+    latitude {35.6890329}
+    longitude {139.7121998}
+    name { Faker::Lorem.characters(number:8) }
+    customer
+  end
+end
