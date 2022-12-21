c14ed21694e4d1340f67df528d906625e9feb525
EC2 Default User <k.nulaw20.gm.9406@gmail.com>
2022-12-21 11:24:26 +0000

    [u]

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -4,4 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'Propertyモデルのテスト', type: :model do
 
+
+
+  describe 'アソシエーションのテスト' do
+
+    context 'favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Customer.reflect_on_association(:favarites).macro).to eq :has_many
+      end
+    end
+
+  end
+
 end
